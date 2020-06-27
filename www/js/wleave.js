84250fe5a41ad6039cc1eba3d7076c215f6fb4e0
agilma <agil.mahendra@gmail.com>
2020-06-27 14:45:25 +0700

    updated

--- www/js/wleave.js
+++ www/js/wleave.js
@@ -1,6 +1,15 @@
+ var lines = new Array()
+ var form = {}
+ var descriptionLine = null,
+    amountLine = null,
+    startLine = null,
+    endLine = null,
+    componentLine = null;
 
-$(function() {
-    this.quota()
+ //  var component = null;
+ 
+ $(function() {
+    // this.quota()
     this.wleave()
 });
 
@@ -11,7 +20,7 @@ function quota (){
     for (let i in ps) {
         oid = ps[i].org_id
     }
-
+    
     $.ajax({
         crossDomain: true,
         type: 'GET',
@@ -26,7 +35,8 @@ function quota (){
         },
         success: function(msg){
             if(msg.status==='success'){
-                $('#quotaWleave').innerHTML(msg.data); //append your new stat
+                console.log('check', $('#quotaWleave'))
+                $('#quotaWleave').html(msg.data); //append your new stat
             }else{
                 alert(msg.message);
             }
@@ -36,7 +46,52 @@ function quota (){
 
 function wleave (){
     const mN = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-      "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"
+    "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"
+];
+
+oid = 0
+let ps = JSON.parse(sessionStorage.getItem('user.works_in_hotel'));
+for (let i in ps) {
+    oid = ps[i].org_id
+}
+
+$.ajax({
+    crossDomain: true,
+    type: 'GET',
+    // make sure you respect the same origin policy with this url:
+    // http://en.wikipedia.org/wiki/Same_origin_policy
+    url: window.localStorage.getItem('base_url')+"/hr/workleave",
+    data: { start: 'first day of this month', end: 'today', org_id : oid },
+    beforeSend: function (xhr) {
+        /* Authorization header */
+        xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('user.jwt'));
+        // xhr.setRequestHeader("X-Mobile", "false");
+    },
+    success: function(msg){
+        if(msg.status==='success'){
+            $(".document").remove(); //remove all the tr's except first ,As you are using it as table headers.            
+            if(!msg.data.data.length){
+                var html = '<div class="item align-center document"><i class="float-center" style="font-size:11px;">tidak ada riwayat pengajuan</i></div>'
+                $('#documentWleave').append(html); //append your new tr
+            }
+            
+            $(msg.data.data).each(function () {
+                var d = new Date(this.date).getDate();
+                var m = mN[new Date(this.date).getMonth()];
+                var Y = new Date(this.date).getFullYear();
+                var html = '<div class="item document"><div class="row"><div class="col-25"><p class="align-center">'+d+' '+m+' '+Y+'</p></div><div class="col"><p class="text-grey-500 wrap"><strong>'+this.at+'</strong><br/>'+this.status+'</p></div></div></div>';
+                $('#documentWleave').append(html); //append your new tr
+            });
+        }else{
+            alert(msg.message);
+        }
+    }
+});
+}
+
+function wleavecomponent() {
+    const mN = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+        "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"
     ];
 
     oid = 0
@@ -50,8 +105,112 @@ function wleave (){
         type: 'GET',
         // make sure you respect the same origin policy with this url:
         // http://en.wikipedia.org/wiki/Same_origin_policy
+        url: window.localStorage.getItem('base_url')+"/hr/component",
+        data: { date: 'today', org_id : oid },
+        beforeSend: function (xhr) {
+            /* Authorization header */
+            xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('user.jwt'));
+            // xhr.setRequestHeader("X-Mobile", "false");
+        },
+        success: function(msg){
+            console.log('check', msg)
+            if(msg.status==='success'){
+                $(msg.data.data).each(function(index, item) {
+                    console.log('check index', index);
+                    console.log('check item', item);
+                    var option = $('<option value="' + item.id + '" data-name="'+ item.title +'">'+ item.title + '</option>');
+                    // var html = '<option value="' + item.id + '" data-item="'+ JSON.stringify(item) +'">'+ item.title + '</option>'
+                    $('#componentLine').append(option);
+                })
+                // $('#category').
+                // $(".document").remove(); //remove all the tr's except first ,As you are using it as table headers.            
+                // if(!msg.data.data.length){
+                //     var html = '<div class="item align-center document"><i class="float-center" style="font-size:11px;">tidak ada riwayat pengajuan</i></div>'
+                //     $('#documentWleave').append(html); //append your new tr
+                // }
+                
+                // $(msg.data.data).each(function () {
+                //     var d = new Date(this.date).getDate();
+                //     var m = mN[new Date(this.date).getMonth()];
+                //     var Y = new Date(this.date).getFullYear();
+                //     var html = '<div class="item document"><div class="row"><div class="col-25"><p class="align-center">'+d+' '+m+' '+Y+'</p></div><div class="col"><p class="text-grey-500 wrap"><strong>'+this.at+'</strong><br/>'+this.status+'</p></div></div></div>';
+                //     $('#documentWleave').append(html); //append your new tr
+                // });
+            }else{
+                alert(msg.message);
+            }
+        }
+    });
+}
+
+function onChangeAmount(elem) {
+    const tmpParent = $(elem).parents('.template');
+    const startLine = $(tmpParent).find('.startLine');
+
+    startLine.removeAttr('disabled')
+
+    if (startLine.val() !== '') {
+      onChangeStart($(tmpParent).find('.startLine'));
+    }
+  }
+
+  function onChangeStart(elem) {
+    const tmpParent = $(elem).parents('.template');
+    const tmpElemAmount = $(tmpParent).find('.amountLine');
+    const tmpAmount = parseInt($(tmpElemAmount).val());
+    const endDate = moment($(elem).val()).add(parseInt(tmpAmount), 'days').format('YYYY-MM-DD');
+
+    $(tmpParent.find('.endLine')).val(endDate);
+  }
+
+function addDocument() {
+    var tmp = $('#template-alert-custom');
+    var section = $('#workleaveLine');
+    var clone = tmp.clone().removeClass('hidden').addClass('white template').css('margin-top', '20px').css('margin-bottom', '20px;').removeAttr('id');
+
+    section.append(clone);
+}
+
+function deleteDocument(elem) {
+    $(elem).parents('.template').remove();
+}
+
+function save() {
+    var tmp = $('#workleaveLine');
+    lines = []
+    tmp.children('.template').each(function(index, item) {
+        const title = $('#titleId').val()
+        const desc = $(item).find('.descriptionLine').val();
+        const amount = $(item).find('.amountLine').val();
+        const start = $(item).find('.startLine').val();
+        const end = $(item).find('.endLine').val();
+        const component = $(item).find('.componentLine');
+        const componentOption = $(component.children('option')[component[0].selectedIndex])
+        
+        lines.push({
+            description: desc,
+            started_at: start,
+            ended_at: end,
+            amount: amount,
+            component_id: component.val(),
+            component_title: componentOption.attr('data-name')
+        })
+
+        form.title = title
+        form.lines = lines
+    })
+
+    $.ajax({
+        crossDomain: true,
+        type: 'POST',
+        // make sure you respect the same origin policy with this url:
+        // http://en.wikipedia.org/wiki/Same_origin_policy
         url: window.localStorage.getItem('base_url')+"/hr/workleave",
-        data: { start: 'first day of this month', end: 'today', org_id : oid },
+        data: { 
+            title: form.title, 
+            lines: form.lines,
+            org_id : oid 
+        },
         beforeSend: function (xhr) {
             /* Authorization header */
             xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('user.jwt'));
@@ -59,24 +218,90 @@ function wleave (){
         },
         success: function(msg){
             if(msg.status==='success'){
-                $(".document").remove(); //remove all the tr's except first ,As you are using it as table headers.            
-                if(!msg.data.data.length){
-                    var html = '<div class="item align-center document"><i class="float-center" style="font-size:11px;">tidak ada riwayat pengajuan</i></div>'
-                    $('#documentWleave').append(html); //append your new tr
-                }
-
-                $(msg.data.data).each(function () {
-                    var d = new Date(this.date).getDate();
-                    var m = mN[new Date(this.date).getMonth()];
-                    var Y = new Date(this.date).getFullYear();
-                    var html = '<div class="item document"><div class="row"><div class="col-25"><p class="align-center">'+d+' '+m+' '+Y+'</p></div><div class="col"><p class="text-grey-500 wrap"><strong>'+this.at+'</strong><br/>'+this.status+'</p></div></div></div>';
-                    $('#documentWleave').append(html); //append your new tr
-                });
+                window.location.href = "workleave.html";
             }else{
                 alert(msg.message);
             }
         }
     });
+}
+
+// function addLine() {
+//     console.log('check selected', $('#template-alert-custom'));
+//     console.log('check description', descriptionLine);
+//     console.log('check amount', amountLine);
+
+    
+//     const desc = $('#descriptionLine').val();
+//     const amount = $('#amountLine').val();
+//     const start = $('#startLine').val();
+//     const end = $('#endLine').val();
+//     const tmpComponent = $('#componentLine');
+//     const component = $(tmpComponent.children('option')[tmpComponent[0].selectedIndex])
+//     console.log('check select', $('#descriptionLine'));
+//     console.log('check val', tmpComponent.val());
+//     console.log('check option', tmpComponent.children('option')[tmpComponent[0].selectedIndex]);
+//     console.log('check option', tmpComponent.children('option').find('selected'));
+//     console.log('check selected', $(tmpComponent.children('option')[tmpComponent[0].selectedIndex]))
+//     lines.push({
+//         desc: desc,
+//         started_at: start,
+//         ended_at: end,
+//         amount: amount,
+//         component_id: tmpComponent.val(),
+//         component_title: component.attr('data-name')
+//     })
+//     console.log('ceechk', lines)
+    
+//     console.log('check component', this.component)
+//     console.log('check component', component.attr('data-name'))
+// }
+
+function test(param) {
+    console.log('check param', param)
+    alert({
+        title: 'Ajukan Cuti',
+        message: 'Harap isikan data berikut',
+        template: 'template-alert-custom',
+        width: '90%',
+        inputs: [
+            {
+                label: 'Test'
+            }
+        ],
+        buttons:[
+            {
+                label: 'Finish',
+                onclick: function() {
+                    console.log('check this', this)
+                    addLine();
+                    // closeAlert();
+                }
+            },
+            {
+                label:'Cancel',
+                onclick: function() {
+                    closeAlert();
+                }
+            }
+        ]
+    })
+}
+
+function loadLine() {
+    console.log('check lines', lines);
+    lines.forEach(function(item, index) {
+        console.log('chekc item', item)
+        console.log('chekc index', index)
+    })
+}
+
+function onFormLine() {
+    window.location.href = "wleaveFormLine.html";
+}
+
+function wleaveTemplate() {
+    
 }
 
 // DEFINED LINKS
